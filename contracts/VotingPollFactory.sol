ee81a0a62d1e147220ed14ce1fb2e0e95368f25a
markdavid000 <davidojukwu59@gmail.com>
2024-02-24 03:23:07 +0100

    test completed

--- contracts/VotingPollFactory.sol
+++ contracts/VotingPollFactory.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.24;
 import "./VotingPoll.sol";
 contract VotingPollFactory {
-    VotingPoll[] votingPollClones;
+    VotingPoll[] public votingPollClones;
 
     function createVotingPollInstance(string memory _question, string[] memory _options) external returns(VotingPoll votingPoll_) {
         votingPoll_ = new VotingPoll(_question, _options);
--- test/Lock.ts
+++ test/Lock.ts
@@ -6,122 +6,119 @@ import { anyValue } from "@nomicfoundation/hardhat-chai-matchers/withArgs";
 import { expect } from "chai";
 import { ethers } from "hardhat";
 
-describe("Lock", function () {
+describe("VotingPollFactory", () => {
   // We define a fixture to reuse the same setup in every test.
   // We use loadFixture to run this setup once, snapshot that state,
   // and reset Hardhat Network to that snapshot in every test.
-  async function deployOneYearLockFixture() {
-    const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-    const ONE_GWEI = 1_000_000_000;
-
-    const lockedAmount = ONE_GWEI;
-    const unlockTime = (await time.latest()) + ONE_YEAR_IN_SECS;
-
+  const deployFactoryVotingPollAndVotingPollFixture = async () => {
     // Contracts are deployed using the first signer/account by default
     const [owner, otherAccount] = await ethers.getSigners();
 
-    const Lock = await ethers.getContractFactory("Lock");
-    const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
+    const VotingPollFactory = await ethers.getContractFactory(
+      "VotingPollFactory"
+    );
+    const votingPollFactory = await VotingPollFactory.deploy();
 
-    return { lock, unlockTime, lockedAmount, owner, otherAccount };
-  }
+    const VotingPoll = await ethers.getContractFactory("VotingPoll");
+    const votingPoll = await VotingPoll.deploy(
+      "Which is the fastest car in the world",
+      ["Bugatti", "Dodge Challenger", "Ferrari", "Lamborghini"]
+    );
 
-  describe("Deployment", function () {
-    it("Should set the right unlockTime", async function () {
-      const { lock, unlockTime } = await loadFixture(deployOneYearLockFixture);
+    return { votingPollFactory, votingPoll, owner, otherAccount };
+  };
 
-      expect(await lock.unlockTime()).to.equal(unlockTime);
-    });
-
-    it("Should set the right owner", async function () {
-      const { lock, owner } = await loadFixture(deployOneYearLockFixture);
-
-      expect(await lock.owner()).to.equal(owner.address);
-    });
-
-    it("Should receive and store the funds to lock", async function () {
-      const { lock, lockedAmount } = await loadFixture(
-        deployOneYearLockFixture
+  describe("Deployment", () => {
+    it("Should check if votingPollFactory address is address(0)", async () => {
+      const { votingPollFactory } = await loadFixture(
+        deployFactoryVotingPollAndVotingPollFixture
       );
 
-      expect(await ethers.provider.getBalance(lock.target)).to.equal(
-        lockedAmount
-      );
+      expect(votingPollFactory.target).is.not.equal(0);
     });
 
-    it("Should fail if the unlockTime is not in the future", async function () {
-      // We don't use the fixture here because we want a different deployment
-      const latestTime = await time.latest();
-      const Lock = await ethers.getContractFactory("Lock");
-      await expect(Lock.deploy(latestTime, { value: 1 })).to.be.revertedWith(
-        "Unlock time should be in the future"
+    it("Should check if votingPoll address is address(0)", async () => {
+      const { votingPoll } = await loadFixture(
+        deployFactoryVotingPollAndVotingPollFixture
+      );
+
+      expect(votingPoll.target).is.not.equal(0);
+    });
+  });
+
+  describe("Instance Creation", () => {
+    it("Should be able to create voting poll instance", async () => {
+      const { votingPollFactory } = await loadFixture(
+        deployFactoryVotingPollAndVotingPollFixture
+      );
+
+      const question = "Which is the fastest car in the world";
+      const options = ["Bugatti", "Dodge Challenger", "Ferrari", "Lamborghini"];
+
+      const votingFactoryTx = await votingPollFactory.createVotingPollInstance(
+        question,
+        options
+      );
+      await votingFactoryTx.wait();
+
+      const pollInstance = await votingPollFactory.getVotingPollInstance();
+
+      expect(pollInstance[0]).to.equal(
+        "0xa16E02E87b7454126E5E10d957A927A7F5B5d2be"
       );
     });
   });
 
-  describe("Withdrawals", function () {
-    describe("Validations", function () {
-      it("Should revert with the right error if called too soon", async function () {
-        const { lock } = await loadFixture(deployOneYearLockFixture);
+  describe("Voting", () => {
+    describe("Validation", () => {
+      it("Should revert if option does not exist", async () => {
+        const { votingPoll } = await loadFixture(
+          deployFactoryVotingPollAndVotingPollFixture
+        );
 
-        await expect(lock.withdraw()).to.be.revertedWith(
-          "You can't withdraw yet"
+        await expect(votingPoll.vote(5)).to.be.revertedWithCustomError(
+          votingPoll,
+          "INVALID_OPTION"
+        );
+
+        await expect(votingPoll.getVotes(5)).to.be.revertedWithCustomError(
+          votingPoll,
+          "INVALID_OPTION"
         );
       });
 
-      it("Should revert with the right error if called from another account", async function () {
-        const { lock, unlockTime, otherAccount } = await loadFixture(
-          deployOneYearLockFixture
+      it("Should check if owner address is address(0)", async () => {
+        const { owner } = await loadFixture(
+          deployFactoryVotingPollAndVotingPollFixture
         );
 
-        // We can increase the time in Hardhat Network
-        await time.increaseTo(unlockTime);
-
-        // We use lock.connect() to send a transaction from another account
-        await expect(lock.connect(otherAccount).withdraw()).to.be.revertedWith(
-          "You aren't the owner"
-        );
-      });
-
-      it("Shouldn't fail if the unlockTime has arrived and the owner calls it", async function () {
-        const { lock, unlockTime } = await loadFixture(
-          deployOneYearLockFixture
-        );
-
-        // Transactions are sent using the first signer by default
-        await time.increaseTo(unlockTime);
-
-        await expect(lock.withdraw()).not.to.be.reverted;
+        expect(owner.address).not.to.equal(0);
       });
     });
-
-    describe("Events", function () {
-      it("Should emit an event on withdrawals", async function () {
-        const { lock, unlockTime, lockedAmount } = await loadFixture(
-          deployOneYearLockFixture
+    describe("Event", () => [
+      it("Should emit an event on successful vote", async () => {
+        const { votingPoll, owner } = await loadFixture(
+          deployFactoryVotingPollAndVotingPollFixture
         );
 
-        await time.increaseTo(unlockTime);
+        const votingTx = await votingPoll.vote(3);
+        await votingTx.wait();
 
-        await expect(lock.withdraw())
-          .to.emit(lock, "Withdrawal")
-          .withArgs(lockedAmount, anyValue); // We accept any value as `when` arg
-      });
-    });
+        expect(votingTx)
+          .to.emit(votingPoll, "Voted")
+          .withArgs(owner.address, 3);
+      }),
+    ]);
+    it("Should Vote successfully", async () => {
+      const { votingPoll } = await loadFixture(
+        deployFactoryVotingPollAndVotingPollFixture
+      );
 
-    describe("Transfers", function () {
-      it("Should transfer the funds to the owner", async function () {
-        const { lock, unlockTime, lockedAmount, owner } = await loadFixture(
-          deployOneYearLockFixture
-        );
+      const votingTx = await votingPoll.vote(3);
+      await votingTx.wait();
 
-        await time.increaseTo(unlockTime);
-
-        await expect(lock.withdraw()).to.changeEtherBalances(
-          [owner, lock],
-          [lockedAmount, -lockedAmount]
-        );
-      });
+      const voteCount = await votingPoll.getVotes(3)
+      expect(voteCount).to.equal(1);
     });
   });
 });
